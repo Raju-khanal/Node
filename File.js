623c8c86bd365fcb4c72fc068af39ecc4b48d5f9
Raju Khanal <rajukhanal933@gmail.com>
2026-05-28 12:26:39 +0545

    FIle handling Sync Async

---
+++ File.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+
+// fs.writeFileSync("./test.txt", `${new Date().toLocaleDateString()} RAJU`);
+// fs.writeFile("./test.txt", `${new Date().toLocaleString()} "Raju khanal Ho"`, (err) => { });
+// const status = fs.statSync("./test.txt");
+// fs.appendFileSync("./test.txt", `\n${new Date().toLocaleString()} "RAJU KHANAL"`);
